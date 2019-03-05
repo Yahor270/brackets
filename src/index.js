08d5e0bfc1623cf602f856cd09e94a5b17f797f4
Yahor270 <270@tut.by>
2019-03-05 12:06:48 +0200

    tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var tst=[], k=' ', act=0;
+  for (i=0; i<str.length; i++) {
+    for (j=0; j<bracketsConfig.length; j++){
+      if (str[i]==bracketsConfig[j][1]){
+       if (tst.length>0 && tst[tst.length-1]==bracketsConfig[j][0]) tst.pop();
+        else tst.push(str[i]);           
+       }
+      else if (str[i]==bracketsConfig[j][0]) tst.push(str[i]);
+    }
+  }    
+  if (tst.length==0) return true;
+  else return false; 
 }
